1ae042b00b6420779e01ce8964114350ffe4a2ca
Andrii-Vekker <vekker304@gmail.com>
2020-12-23 00:22:56 +0200

    take

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './styles.css';
+ const debounce = require('lodash.debounce');
+import './notifications'
 
 
+//========================input=============================//
+
+const inputRef = document.querySelector('#name-input');
+const nameRef = document.querySelector('#name-output')
+inputRef.addEventListener('input', handleInputChange)
+
+
+
+
+function handleInputChange(event) {
+
+    inputRef.value = nameRef.textContent = event.target.value;
+}
+const ref = {
+    inputRef: document.querySelector('#name-input'),
+    spanRef: document.querySelector('#name-output')
+};
+ref.inputRef.addEventListener('input', handleTextInput);
+function handleTextInput(event) {
+    ref.inputRef.value = ref.spanRef.textContent = event.target.value;
+};
+
+
+
+//============================
