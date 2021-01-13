5f66cbb644378383d022de72d333a803ef812f4e
Andrii-Vekker <vekker304@gmail.com>
2021-01-13 12:25:43 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 
+//import './styles.css';
+// import countriesTpl from './templates/countries.hbs'
+// import debounce from 'lodash.debounce';
+import './saas/styles.scss';
+
 import debounce from 'lodash.debounce';
-import { alert, defaultModules } from '@pnotify/core';
-import { defaults } from '@pnotify/core';
-import countriesTpl from './templates/countries.hbs'
-import './styles.css';
-//import './notifications';
+import refs from './js/refs';
+import fetchCountry from './js/fetch-countries';
+import showQueryResults from './js/handle-query-resuts';
+
+refs.input.addEventListener('input', debounce(onInput, 500));
+refs.clearBtn.addEventListener('click', onBtnClick);
+
+function onInput(event) {
+  const searchQuery = event.target.value;
+  refs.countriesContainer.innerHTML = '';
+
+  if (searchQuery) {
+    fetchCountry(searchQuery).then(showQueryResults);
+  }
+}
+
+function onBtnClick() {
+  refs.input.value = '';
+  refs.countriesContainer.innerHTML = '';
+}
 
 //========================input=============================//
 
-let inputRef = document.querySelector('#name-input');
- const nameRefMurkup = document.querySelector('#name-output')
+// let inputRef = document.querySelector('#name-input');
+//  const nameRefMurkup = document.querySelector('#name-output')
 
- inputRef.addEventListener('input', debounce(handleTextInput), 3500);
-function handleTextInput(event) {
-    let nameCountry = '';
-    nameCountry = event.target.value
-     nameRefMurkup.textContent = nameCountry
+//  inputRef.addEventListener('input', debounce(handleTextInput), 3500);
+// function handleTextInput(event) {
+//     let nameCountry = '';
+//     nameCountry = event.target.value
+//      nameRefMurkup.textContent = nameCountry
 
-fetch(`https://restcountries.eu/rest/v2/name/${nameCountry}`)
-    .then(response => response.json())
-    .then(nameCountry => {
-        console.log(nameCountry)
-        const murkup = countriesTpl(nameCountry);
-        console.log(murkup)
-        nameRefMurkup.insertAdjacentHTML('beforeend', murkup)
-    });
- };
+// fetch(`https://restcountries.eu/rest/v2/name/${nameCountry}`)
+//     .then(response => response.json())
+//     .then(nameCountry => {
+//         console.log(nameCountry)
+//         const murkup = countriesTpl(nameCountry);
+//         console.log(murkup)
+//         nameRefMurkup.insertAdjacentHTML('beforeend', murkup)
+//     });
+//  };
 
 
--- src/notifications.js
+++
@@ -1,5 +0,0 @@
-import {error, Stack} from'@pnotify/core';
-import"@pnotify/core/dist/PNotify.css";
-import"@pnotify/core/dist/BrightTheme.css";
-
- 
